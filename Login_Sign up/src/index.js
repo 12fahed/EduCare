aaf932fe2ea061b6dac06477f3e1a69c68bf65d0
Fahed Khan <127182880+12fahed@users.noreply.github.com>
2024-02-24 09:06:50 +0530

    Login Sign up

---
+++ Login_Sign up/src/index.js
@@ -0,0 +1,72 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const hbs = require("hbs")
+const collection = require("./mongodb")
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
+const bcryptjs = require("bcryptjs")
+const { constants } = require("buffer")
+
+const tempelatePath = path.join(__dirname, '../tempelates')
+
+app.use(express.json())
+app.use(cookieParser())
+app.set("view engine", "hbs")
+app.set("views", tempelatePath)
+app.use(express.urlencoded({extended: false}))
+
+async function hashpass(password){
+
+    const res = await bcryptjs.hash(password, 10)
+    return res
+}
+
+async function compare(userPass, hashPass){
+
+    const res = await bcryptjs.compare(userPass, hashPass)
+    return res
+}
+
+app.get("/", (req, res)=>{
+    res.render("login")
+})
+
+app.get("/signup", (req, res)=>{
+    res.render("signup")
+})
+
+app.post("/signup", async (req, res)=>{
+    const data = {
+        name: req.body.name,
+        password: await hashpass(req.body.password)
+    }
+
+    await collection.insertMany([data])
+
+    res.render("home")
+})
+
+app.post("/login", async (req, res)=>{
+
+    try{
+        const check = await collection.findOne({name: req.body.name})
+        const passCheck = await compare(req.body.password, check.password)
+
+        if(check && passCheck){
+            
+            res.render("home")
+        }
+        else{
+            res.send("Wrong Password")
+        } 
+    }
+    catch{
+        res.send("Wrong Details")
+    }
+    
+})
+
+app.listen(3000, () =>{
+    console.log("Port Connected");
+})
---
+++ Login_Sign up/src/mongodb.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://fahed:fahed12@admin.gd1pah0.mongodb.net/HTA")
+.then(()=>{
+    console.log("Mongo DB Connected");
+})
+.catch(()=>{
+    console.log("Failed To Connect");
+})
+
+
+const LogInScheama = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const collection = new mongoose.model("userInfo", LogInScheama)
+
+module.exports = collection
